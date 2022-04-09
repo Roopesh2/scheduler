b94f40245b3223fde91dc182b44df1bdc6e7829e
Roopesh <62459820+Roopesh2@users.noreply.github.com>
2022-04-09 12:43:31 +0530

    Create colors.js

---
+++ colors.js
@@ -0,0 +1,40 @@
+window["cols"] = {
+  tomato: "#dd5236",
+  tangerine: "#e5683f",
+  banana: "#e1b54f",
+  basal: "#489160",
+  sage: "#56b080",
+  peacock: "#4d99d3",
+  blueberry: "#6f72c2",
+  lavender: "#828bc2",
+  grape: "#a75aba",
+  flamingo: "#d7837a",
+  graphite: "#7c7c7c",
+  blues: "#4d99d3",
+  grey: "#888",
+  orange: '#ff8000'
+}
+
+window['flatui'] = {
+  sage: '#1abc9c',
+  green: '#2ecc71',
+  blue: '#3498db',
+  purple: '#9b59b6',
+  dark: '#34495e',
+  
+  sage2: '#16a085',
+  green2: '#27ae60',
+  blue2: '#2980b9',
+  purple2: '#8e44ad',
+  dark2: '#2c3e50',
+  
+  yellow: '#f1c40f',
+  orange: '#e67e22',
+  red: '#e74c3c',
+  white: '#95a5a6',
+  
+  yellow2: '#f39c12',
+  orange2: '#d35400',
+  red2: '#c0392b',
+  white2: '#bdc3c7',
+}
